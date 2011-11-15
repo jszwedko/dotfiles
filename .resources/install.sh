096a0713e7f68290932560c1fcfe5e8082855e1c
jszwedko <jesse.szwedko@gmail.com>
2011-11-14 20:25:18 -0500

    Added xmonad and xmobarrc configs, Xdefaults, and xsession. Also updated install script

--- .resources/install.sh
+++ .resources/install.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
-read -p '$' -n 1
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo
-    if [ ! -d "~/dotfiles.bak" ]; then
-        mkdir ~/dotfiles.bak
-    fi
-    mv ~/.zshrc ~/dotfiles.bak
-    mv ~/.vimrc ~/dotfiles.bak
-    mv ~/.vim ~/dotfiles.bak
-    mv ~/.tmux.conf ~/dotfiles.bak
-    mv ~/.ssh/config ~/dotfiles.bak/ssh_config
-    mv ~/.gitconfig ~/dotfiles.bak
-    echo . ~/dotfiles/bashrc > ~/.bashrc
-    ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-    ln -s ~/dotfiles/vimrc ~/.vimrc
-    ln -s ~/dotfiles/vim ~/.vim
-    ln -s ~/dotfiles/zshrc ~/.zshrc
-    ln -s ~/dotfiles/ssh/config ~/.ssh/config
-    ln -s ~/gitconfig ~/.gitconfig
-else
-    echo && echo 'Okay, no changes made.'
+echo
+if [ ! -d "~/dotfiles.bak" ]; then
+    mkdir ~/dotfiles.bak
 fi
+mv ~/.zshrc ~/dotfiles.bak
+mv ~/.vimrc ~/dotfiles.bak
+mv ~/.vim ~/dotfiles.bak
+mv ~/.tmux.conf ~/dotfiles.bak
+mv ~/.ssh/config ~/dotfiles.bak/ssh_config
+mv ~/.gitconfig ~/dotfiles.bak
+mv ~/.xmobarrc ~/dofiles.bak
+mv ~/.xmonad/xmonad.hs ~/dotfiles.bak
+mv ~/.Xdefaults ~/dotfiles.bak
+mv ~/.xsession ~/dotfiles.bak
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/ssh/config ~/.ssh/config
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s ~/dotfiles/xmobarrc ~/.xmobarrc
+ln -s ~/dotfiles/Xdefaults ~/.Xdefaults
+ln -s ~/dotfiles/xsession ~/.xsession
